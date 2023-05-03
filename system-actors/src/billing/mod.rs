3c8fa6f4f10e5b97417c6080aaa8a27b419577de
Jacky.li <liyangwood@gamil.com>
2023-05-03 21:12:01 +0800

    feat: update billing actor defination

--- system-actors/src/billing/mod.rs
+++ system-actors/src/billing/mod.rs
@@ -2,10 +2,12 @@ use std::time::Duration;
 
 use serde::{Deserialize, Serialize};
 use tea_codec::serde::TypeId;
+use tea_runtime_codec::tapp::AccountId;
 
 pub mod error;
 
-pub const SETTLEMENT_INTERVAL: Duration = Duration::from_secs(600);
+pub const NAME: &[u8] = b"tea:billing";
+pub const SETTLEMENT_INTERVAL: Duration = Duration::from_secs(60);
 
 #[derive(Debug, Clone, Serialize, Deserialize, TypeId)]
 #[response(())]
@@ -14,3 +16,10 @@ pub struct RegisterGasFeeHandlerRequest;
 #[derive(Debug, Clone, Serialize, Deserialize, TypeId)]
 #[response(())]
 pub struct HandleGasFeeRequest(pub Vec<u8>);
+
+#[derive(Debug, Clone, Serialize, Deserialize, TypeId)]
+#[response(())]
+pub struct GasFeeCostRequest {
+	pub acct: AccountId,
+	pub gas: u64,
+}
