0be61eebf011d024e5343f5b533368dda0f6b770
Yan Mingzhi <realraindust@gmail.com>
2023-05-05 09:45:44 +0800

    feat: remove clean expired request

--- system-actors/src/keyvalue/mod.rs
+++ system-actors/src/keyvalue/mod.rs
@@ -1,13 +1,4 @@
-use serde::{Deserialize, Serialize};
-use tea_codec::pricing::Priced;
-use tea_codec::serde::TypeId;
-
 pub mod actions;
 pub mod error;
 
 pub const NAME: &[u8] = b"tea:keyvalue";
-
-#[derive(Debug, Clone, Serialize, Deserialize, TypeId, Priced)]
-#[price(0)]
-#[response(())]
-pub struct CleanExpiredRequest;
