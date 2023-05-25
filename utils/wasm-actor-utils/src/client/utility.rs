f8b24d946dbab35e50682485ffb47a710cdc7763
geopor <86096370+geopor@users.noreply.github.com>
2023-05-25 16:18:24 -0700

    Update utility.rs

--- utils/wasm-actor-utils/src/client/utility.rs
+++ utils/wasm-actor-utils/src/client/utility.rs
@@ -2,7 +2,7 @@ use crate::client::Result;
 use tea_codec::ResultExt;
 use tea_runtime_codec::tapp::Account;
 
-/// Parse address string to Account(H160)
+/// Parse address string to Ethereum account address (H160)
 pub fn parse_to_acct(address: &str) -> Result<Account> {
 	address.parse().err_into()
 }
