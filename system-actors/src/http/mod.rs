e87135df642942fe520cc75afbbcf965a76ceea8
Jacky.li <liyangwood@gamil.com>
2023-07-08 16:16:56 +0800

    add http oracle request

--- system-actors/src/http/mod.rs
+++ system-actors/src/http/mod.rs
@@ -1,5 +1,22 @@
+use serde::{Deserialize, Serialize};
+use tea_codec::serde::TypeId;
+
 pub mod error;
 
 pub const NAME: &[u8] = b"tea:http";
 
 pub use tea_runtime_codec::runtime::http::{HttpRequest, HttpResponse};
+
+#[derive(Debug, Clone, Serialize, Deserialize, TypeId)]
+pub struct OracleHttpRequest {
+	pub method: String,
+	pub url: String,
+	pub headers: Option<Vec<(String, String)>>,
+	pub payload: Option<String>,
+}
+
+/// Base response from sending an oracle http request via tea system.
+#[derive(Debug, Clone, Serialize, Deserialize, TypeId)]
+pub struct OracleHttpResponse {
+	pub text: String,
+}
