deee93c1431c4a270e119741f4cde490cd1fe296
Yan Mingzhi <realraindust@gmail.com>
2023-09-05 06:51:13 +0000

    chore: add auth field

--- system-actors/src/payment_channel/txns.rs
+++ system-actors/src/payment_channel/txns.rs
@@ -7,24 +7,29 @@ pub enum PaymentChannelTxn {
 	OpenChannel {
 		item: ChannelItem,
 		payer_acc_sig: Vec<u8>,
+		auth_b64: String,
 	},
 	PayerEarlyTerminate {
 		channel_id: ChannelId,
 		payer_sig: Vec<u8>,
+		auth_b64: String,
 	},
 	PayerTerminate {
 		channel_id: ChannelId,
 		payer_sig: Vec<u8>,
+		auth_b64: String,
 	},
 	PayerRefill {
 		channel_id: ChannelId,
 		refill_amount: Balance,
 		payer_sig: Vec<u8>,
+		auth_b64: String,
 	},
 	UpdatePayment {
 		channel_id: ChannelId,
 		payment_update_sig: Vec<u8>,
 		new_fund_remaining: Balance,
 		close_channel: bool,
+		auth_b64: String,
 	},
 }
